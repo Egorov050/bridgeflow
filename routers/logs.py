eda79faf301d9a54c7d43646c34d3cec8c2e62c5
Egorov050 <115251460+Egorov050@users.noreply.github.com>
2026-02-23 20:07:31 +0300

    Filter logs by user_id

--- routers/logs.py
+++ routers/logs.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Log
+from models import Log, Bridge
 
 router = APIRouter()
 
 @router.get("/")
-def get_all_logs(db: Session = Depends(get_db)):
+def get_all_logs(user_id: int = 0, db: Session = Depends(get_db)):
+    if user_id:
+        bridge_ids = [b.id for b in db.query(Bridge).filter(Bridge.user_id == user_id).all()]
+        return db.query(Log).filter(Log.bridge_id.in_(bridge_ids)).order_by(Log.triggered_at.desc()).limit(50).all()
     return db.query(Log).order_by(Log.triggered_at.desc()).limit(50).all()
 
 @router.get("/{bridge_id}")
 def get_logs_by_bridge(bridge_id: int, db: Session = Depends(get_db)):
-    return db.query(Log).filter(
-        Log.bridge_id == bridge_id
-    ).order_by(Log.triggered_at.desc()).all()
+    return db.query(Log).filter(Log.bridge_id == bridge_id).order_by(Log.triggered_at.desc()).all()
