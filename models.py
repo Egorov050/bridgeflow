03243082c7fb18359ace614d916902cb6c8828c2
Egorov050 <115251460+Egorov050@users.noreply.github.com>
2026-02-24 13:52:42 +0300

    Fix duplicate User class in models.py

--- models.py
+++ models.py
@@ -29,15 +29,7 @@ class Log(Base):
     triggered_at = Column(DateTime, server_default=func.now())
 
 
-class User(Base):
-    __tablename__ = "users"
-    __table_args__ = {'extend_existing': True}
 
-    id            = Column(Integer, primary_key=True, index=True)
-    email         = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
-    created_at    = Column(DateTime, server_default=func.now())
-    
     
     
 class User(Base):
