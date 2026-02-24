0f6f6dc5450eeab8993556d84c8994913ee3ff3a
Egorov050 <115251460+Egorov050@users.noreply.github.com>
2026-02-24 13:48:54 +0300

    Fix duplicate table definition

--- models.py
+++ models.py
@@ -31,6 +31,7 @@ class Log(Base):
 
 class User(Base):
     __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
 
     id            = Column(Integer, primary_key=True, index=True)
     email         = Column(String, unique=True, nullable=False)
